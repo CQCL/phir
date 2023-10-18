af664d9e90d4fd856853b60b0d8c0f24d3111f67
Kartik Singhal <kartik.singhal@quantinuum.com>
2023-10-18 13:59:26 -0500

    Update copyright, add CPY ruff rule

--- phir/cli.py
+++ phir/cli.py
@@ -1,11 +1,8 @@
 ##############################################################################
 #
-# (c) 2023 @ Quantinuum LLC. All Rights Reserved.
-# This software and all information and expression are the property of
-# Quantinuum LLC, are Quantinuum LLC Confidential & Proprietary,
-# contain trade secrets and may not, in whole or in part, be licensed,
-# used, duplicated, disclosed, or reproduced for any purpose without prior
-# written permission of Quantinuum LLC.
+# Copyright (c) 2023 Quantinuum LLC All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
 #
 ##############################################################################
 
